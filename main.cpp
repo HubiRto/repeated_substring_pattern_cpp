ed0bac0c2aaada0be1c1419416518888ce090e01
Hubert Rybicki <hubert.rybicki.hr@gmail.com>
2023-04-26 21:07:06 +0200

    Poprawiony kod

--- main.cpp
+++ main.cpp
@@ -2,23 +2,20 @@
 
 int main() {
     std::string s = "ab";
-    bool isSubstring = true;
-
     char firstLetterOfPattern = s.at(0);
     std::string pattern;
     pattern.push_back(firstLetterOfPattern);
-
     int counter = 1;
     while (s[counter] != firstLetterOfPattern) {
         pattern.push_back(s[counter]);
         counter++;
     }
 
-    for (int i = 0; i < counter; i++) {
-        if(s[i+counter] != pattern[i]) isSubstring = false;
+    std::cout << "Pattern: " << pattern << std::endl;
+    if(pattern >= s){
+        std::cout << 0;
+    }else {
+        std::cout << (s.substr(pattern.size() - 1, s.size()).find(pattern) != -1);
     }
-
-    std::cout << isSubstring;
-
     return 0;
 }
